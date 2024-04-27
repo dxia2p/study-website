241a630c52b7eb2cbb20e5073b6847014e1db541
dxia2p <71045728+dxia2p@users.noreply.github.com>
2024-04-27 17:22:13 -0600

    began working on adding flashcards

--- Front_End/FlashcardsPage/flashcards_script.js
+++ Front_End/FlashcardsPage/flashcards_script.js
@@ -4,11 +4,31 @@ const addFlashcardButton = document.getElementById("add-flashcard-button");
 
 addFlashcardButton.addEventListener("click", addFlashcard);
 
+class Flashcard{
+    static flashcardsArr = [];
+    constructor(id, htmlElement){
+        this.id = id;
+        this.htmlElement = htmlElement;
+        Flashcard.flashcardsArr.push(this);
+    }
+}
+
 function addFlashcard(){
-    para = document.createElement("p");
-    node = document.createTextNode("This is new");
-    para.appendChild(node);
+    termDefDiv = document.createElement("div");
+
+    termText = document.createTextNode("Term: ");
+    term = document.createElement("input");
+    defText = document.createTextNode("Definition: ");
+    def = document.createElement("input"); // def stands for definition
+    deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+
+    termDefDiv.appendChild(termText);
+    termDefDiv.appendChild(term);
+    termDefDiv.appendChild(defText);
+    termDefDiv.appendChild(def);
+    termDefDiv.appendChild(deleteButton);
     
-    flashcardsList.appendChild(para);
+    flashcardsList.appendChild(termDefDiv);
 }
 
